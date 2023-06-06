f52b3a38fc73721a51bc36794fac4c6e15c025fa
NikolajT84 <97594388+NikolajT84@users.noreply.github.com>
2023-06-06 11:15:43 +0200

    Karate graph

--- GraphClustering/LargeGraphs/GraphImport.py
+++ GraphClustering/LargeGraphs/GraphImport.py
@@ -5,9 +5,11 @@ import networkx as nx
 import os
 
 # Karate club graph
-G = nx.karate_club_graph()
-Adj_karate = nx.adjacency_matrix(G).todense()
-Adj_karate = Adj_karate>0
+# G = nx.karate_club_graph()
+# Adj_karate = nx.adjacency_matrix(G).todense()
+# Adj_karate = np.array(Adj_karate>0,dtype=int)
+
+Adj_karate = pd.read_csv("Adj_karate.csv", header=None).to_numpy()
 
 # C. elegans graph
-Adj_celegans = pd.read_csv("../GraphClustering/celegans277/celegans277matrix.csv").to_numpy()
+#Adj_celegans = pd.read_csv("../GraphClustering/celegans277/celegans277matrix.csv").to_numpy()
