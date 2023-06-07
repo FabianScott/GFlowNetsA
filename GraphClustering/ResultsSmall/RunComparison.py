601799865bfa417b0a0162e8db99d90ee0313d3d
Fabian Scott <toellerandet@gmail.com>
2023-06-07 21:47:18 +0200

    .

--- GraphClustering/ResultsSmall/RunComparison.py
+++ GraphClustering/ResultsSmall/RunComparison.py
@@ -3,4 +3,4 @@ from GraphClustering.Core.Core import compare_results_small_graphs
 if __name__ == '__main__':
     N = 6
     max_epochs = 300
-    compare_results_small_graphs(filename=f'Comparison_{max_epochs}_{N}.txt', min_N=N, max_N=N, max_epochs=max_epochs)
+    compare_results_small_graphs(filename=f'Comparison_test_{max_epochs}_{N}.txt', min_N=2, max_N=5, max_epochs=max_epochs, use_new_graph_for_test=True)
