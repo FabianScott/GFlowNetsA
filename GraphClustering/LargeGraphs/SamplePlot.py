96d34bbe036220820f9f56e48b32ea1c5aca9731
Fabian Scott <toellerandet@gmail.com>
2023-06-12 08:24:59 +0200

    Samples downloaded

---
+++ GraphClustering/LargeGraphs/SamplePlot.py
@@ -0,0 +1,10 @@
+from GraphClustering.Core.Core import GraphNet
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+
+if __name__ == '__main__':
+    net = GraphNet(n_nodes=34)
+    
+
