708121770af4f0b85bef17ea1b35bfa4066afe85
Fabian Scott <toellerandet@gmail.com>
2023-06-12 14:07:22 +0200

    Sample plot documentet

--- GraphClustering/LargeGraphs/SamplePlot.py
+++ GraphClustering/LargeGraphs/SamplePlot.py
@@ -9,6 +9,16 @@ from scipy import stats
 
 
 def compareIRMSamples(tensors: list, nbins=100, names=None, filenameSave='', title=''):
+    """
+    Plot a histogram of clusterings per IRM values.
+    Can also save the plots.
+    :param tensors:
+    :param nbins:
+    :param names:
+    :param filenameSave:
+    :param title:
+    :return:
+    """
     if names is None:
         names = [str(el) for el in range(1, 1 + len(tensors))]
     # tensorsFlat = torch.concat(tensors, dim=0)
@@ -26,7 +36,7 @@ def compareIRMSamples(tensors: list, nbins=100, names=None, filenameSave='', tit
         IRM_list = np.array(IRM_list)
         sort_idxs.append(np.argsort(IRM_list))
         IRM_list = sorted(IRM_list)
-        plt.hist(IRM_list, label=name, bins=nbins)
+        plt.hist(IRM_list, label=name, bins=nbins, alpha=0.6)
         IRM_lists.append(IRM_list)
         print(f'For {name} we find \tMean: {np.mean(IRM_list)}\tMode: {stats.mode(IRM_list)}\tMax: {np.max(IRM_list)}')
     plt.ylabel('Count')
@@ -40,7 +50,16 @@ def compareIRMSamples(tensors: list, nbins=100, names=None, filenameSave='', tit
     return IRM_lists, sort_idxs
 
 
-def getTopClusterings(states, n=10, csvFilename=''):  # , plot=False, saveFilename='', title='', nbins=100):
+def getTopClusterings(states, n=10, csvFilename=''):  # , plot=False, saveFilename='', title='', nbins=100)
+    """
+    Given an iterable of tensors that represent states,
+    return the top 10 most common states and potentially
+    save them as csv files
+    :param states:
+    :param n:
+    :param csvFilename:
+    :return:
+    """
     def return_0():
         return 0
 
@@ -81,26 +100,28 @@ def getTopClusterings(states, n=10, csvFilename=''):  # , plot=False, saveFilena
 if __name__ == '__main__':
     run_Gibbs = False  # There is a saved run for 10_000 samples in this folder
     epochs = 0
+    topClustersList = []
+    for epochs in range(0, 500, 100):
+        net = GraphNet(n_nodes=34)
+        fname = f'Data/KarateResults_100_500_10000_o_Samples_{epochs}.pt'
+        netSamples = net.load_samples(fname)
 
-    net = GraphNet(n_nodes=34)
-    fname = f'Data/KarateResults_100_500_10000_o_Samples_{epochs}.pt'
-    netSamples = net.load_samples(fname)
+        # Load the adjacency matrix to create the state vector, used by the plotting function
+        Adj_karate = torch.tensor(pd.read_csv("Adj_karate.csv", header=None, dtype=int).to_numpy())
 
-    # Load the adjacency matrix to create the state vector, used by the plotting function
-    Adj_karate = torch.tensor(pd.read_csv("Adj_karate.csv", header=None, dtype=int).to_numpy())
+        if run_Gibbs:
+            gibbsSamples = Gibbs_sample_torch(torch.tensor(Adj_karate, dtype=torch.float32), T=len(netSamples) * 2,
+                                              return_clustering_matrix=True)
+            gibbsSamplesPlotable = [torch.concat((Adj_karate.flatten(), gibbsSample.flatten())) for gibbsSample in gibbsSamples]
+        else:
+            gibbsSamplesPlotable = net.load_samples('Data/GibbsSamples_10000.pt')
 
-    if run_Gibbs:
-        gibbsSamples = Gibbs_sample_torch(torch.tensor(Adj_karate, dtype=torch.float32), T=len(netSamples) * 2,
-                                          return_clustering_matrix=True)
-        gibbsSamplesPlotable = [torch.concat((Adj_karate.flatten(), gibbsSample.flatten())) for gibbsSample in gibbsSamples]
-    else:
-        gibbsSamplesPlotable = net.load_samples('GibbsSamples_10000.pt')
+        torch.save(gibbsSamplesPlotable, 'Data/GibbsSamples_10000.pt')
+        I = compareIRMSamples([netSamples, gibbsSamplesPlotable],
+                              names=['GFlowNet', 'Gibbs Sampler'],
+                              title=f'Histogram of log IRM values for GFlowNet vs GibbsSampler\non Zachary Karate Club graph after {epochs} epochs',
+                              filenameSave=f'Plots/comparisonGraph10000Samples_{epochs}.png')
 
-    torch.save(gibbsSamplesPlotable, 'GibbsSamples_10000.pt')
-    I = compareIRMSamples([netSamples, gibbsSamplesPlotable],
-                          names=['GFlowNet', 'Gibbs Sampler'],
-                          title=f'Histogram of log IRM values for GFlowNet vs GibbsSampler\non Zachary Karate Club graph after {epochs} epochs',
-                          filenameSave=f'comparisonGraph10000Samples_{epochs}.png')
-
-    topClustersNet = getTopClusterings(netSamples, n=5, csvFilename='TopClusteringsGFlowNet.csv')
-    topClustersGibbs = getTopClusterings(gibbsSamplesPlotable, n=5, csvFilename='TopClusteringsGibbs.csv')
+        topClustersNet = getTopClusterings(netSamples, n=5, csvFilename=f'Data/TopClusteringsGFlowNet_{epochs}.csv')
+        topClustersGibbs = getTopClusterings(gibbsSamplesPlotable, n=5, csvFilename=f'Data/TopClusteringsGibbs_{epochs}.csv')
+        topClustersList.append([topClustersNet, topClustersGibbs])
