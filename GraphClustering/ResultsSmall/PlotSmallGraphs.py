50227d391e187f8f5fd717accf2111d9db2e5830
Fabian Scott <toellerandet@gmail.com>
2023-06-14 13:56:03 +0200

    It's all coming together

--- GraphClustering/ResultsSmall/PlotSmallGraphs.py
+++ GraphClustering/ResultsSmall/PlotSmallGraphs.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    alpha = .8
+    alpha = 1.
     df = pd.read_csv('Data/New_Final_Comparison_test_o_101_1_2_5.txt', sep=',')
     df2 = pd.read_csv('Data/New_Final_Comparison_test_o_101_1_6_6.txt', sep=',')
     df3 = pd.read_csv('Data/New_Final_Comparison_test_o_101_1_2_5_TEST.csv', sep=',')
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     plt.savefig('Plots/ErrorPlot6.png')
     plt.show()
 
-    for i, row in enumerate(df.values[:-1]):
+    for i, row in enumerate(df3.values[:-1]):
         plt.plot(row[:-1], label=f'{i+2}', alpha=alpha)
     plt.legend()
     plt.title('GFlowNet Error for randomly generated graph of 2-4 nodes over time')
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     plt.savefig('Plots/TransferErrorPlot2_4.png')
     plt.show()
 
-    plt.plot(df.values[-1], label=f'5', alpha=alpha)
+    plt.plot(df3.values[-1][1:], label=f'5', alpha=alpha)
     plt.legend()
     plt.title('GFlowNet Error for randomly generated graph of 5 nodes over time')
     plt.xlabel('Epochs')
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     plt.savefig('Plots/TransferErrorPlot5.png')
     plt.show()
 
-    plt.plot(df2.values[-1], label=f'6', alpha=alpha)
+    plt.plot(df4.values[-1][1:], label=f'6', alpha=alpha)
     plt.legend()
     plt.title('GFlowNet Error for randomly generated graph of 6 nodes over time')
     plt.xlabel('Epochs')
